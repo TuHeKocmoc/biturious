027630f167c3dcb0665b0cb6c6a32b2f59270f42
TuHeKocmoc <nixlle@rambler.ru>
2021-11-13 16:01:14 +0300

    Optimization

--- binance_api.py
+++ binance_api.py
@@ -13,17 +13,15 @@ def override_where():
 if os.path.exists('settings.txt'):
     file = open('settings.txt', 'r')
     data = file.readlines()
+    try:
+        apikey = data[2].strip()
+        secretkey = data[3].strip()
+        apikey = apikey[apikey.find('=') + 1:]
+        secretkey = secretkey[secretkey.find('=') + 1:]
+    except:
+        apikey = ' '
+        secretkey = ''
 else:
-    data = ['', '', '', '']
-apikey = ' '
-secretkey = ''
-
-try:
-    apikey = data[2].strip()
-    apikey = apikey[apikey.find('=') + 1:]
-    secretkey = data[3].strip()
-    secretkey = secretkey[secretkey.find('=') + 1:]
-except:
     apikey = ' '
     secretkey = ''
 
