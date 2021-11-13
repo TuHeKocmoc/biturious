51e9585654feef23c2115b7ee9c772d28acb4ce8
TuHeKocmoc <nixlle@rambler.ru>
2021-11-13 14:28:16 +0300

    Fixes

--- binance_api.py
+++ binance_api.py
@@ -1,6 +1,17 @@
 from binance.client import Client
 import pathlib
 import os
+import sys
+
+import sys, os
+
+
+def override_where():
+    """ overrides certifi.core.where to return actual location of cacert.pem"""
+    # change this to match the location of cacert.pem
+    return os.path.abspath("cacert.pem")
+
+
 
 path = str(pathlib.Path(__file__).parent.resolve()) + '\settings.txt'
 if os.path.exists(path):
@@ -19,6 +30,21 @@ try:
 except:
     apikey = ' '
     secretkey = ''
+
+# is the program compiled?
+if hasattr(sys, "frozen"):
+    import certifi.core
+
+    os.environ["REQUESTS_CA_BUNDLE"] = override_where()
+    certifi.core.where = override_where
+
+    # delay importing until after where() has been replaced
+    import requests.utils
+    import requests.adapters
+    # replace these variables in case these modules were
+    # imported before we replaced certifi.core.where
+    requests.utils.DEFAULT_CA_BUNDLE_PATH = override_where()
+    requests.adapters.DEFAULT_CA_BUNDLE_PATH = override_where()
 
 client = Client(
     apikey,
