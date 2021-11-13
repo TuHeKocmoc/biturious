65d7cd079f41dc7605004f98264d6fa4ce12eebf
TuHeKocmoc <nixlle@rambler.ru>
2021-11-13 14:14:40 +0300

    Downgrade

--- binance_api.py
+++ binance_api.py
@@ -1,25 +1,25 @@
 from binance.client import Client
 import pathlib
-import os
 
 path = str(pathlib.Path(__file__).parent.resolve()) + '\settings.txt'
-
-if os.path.exists(path):
+if os.path.exists(self.path):
     file = open(path, 'r')
     data = file.readlines()
-    try:
-        apikey = data[2].strip()
-        apikey = apikey[apikey.find('=') + 1:]
-        secretkey = data[3].strip()
-        secretkey = secretkey[secretkey.find('=') + 1:]
-    except:
-        apikey = ' '
-        secretkey = ''
 else:
+    data = ['', '', '', '']
+apikey = ' '
+secretkey = ''
+
+try:
+    apikey = data[2].strip()
+    apikey = apikey[apikey.find('=') + 1:]
+    secretkey = data[3].strip()
+    secretkey = secretkey[secretkey.find('=') + 1:]
+except:
     apikey = ' '
     secretkey = ''
 
-try:
-    client = Client(apikey, secretkey)
-except:
-    pass
+client = Client(
+    apikey,
+    secretkey
+)
